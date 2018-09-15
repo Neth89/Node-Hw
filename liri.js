298ec62fd73848c750b14f443b769a7086ad704b
Solina Neth <solina.neth89@gmail.com>
2018-09-14 17:40:13 -0700

    update

--- liri.js
+++ liri.js
@@ -1,3 +1,35 @@
 require("dotenv").config();
-   var Spotify = require('node-spotify-api');
-   var keys = require('./keys.js');
+var Spotify = require('node-spotify-api');
+var keys = require('./keys.js');
+var spotify = new Spotify(keys.spotify);
+var inquirer = require("inquirer");
+var options1 = "";
+var options2 = ""
+inquirer.prompt([{
+    type: "list",
+    message: "choose: ",
+    choices: ["concert", "movies", "spotify", "dothis"],
+    name: "type1"
+},
+{
+    type: "input",
+    message: "Search:",
+    name: "type2"
+}
+]).then(answers => {
+    options1 = answers.type1;
+    options2 = answers.type2;
+    spotifyThis();
+});
+
+function spotifyThis() {
+    var songName = options2;
+    spotify.search({ type: 'track', query: songName }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        console.log(JSON.stringify(data));
+    });
+
+}
